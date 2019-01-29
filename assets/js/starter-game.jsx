4027c84fada9735051eb9132982f02d7e443df1a
Ben Romine <elementnumber35@gmail.com>
2019-01-29 11:22:15 -0500

    Fixed comments being rendered as text

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -104,60 +104,60 @@ class Starter extends React.Component {
     };
   }
 
-  // Call this function when choosing a tile.
+  {/* Call this function when choosing a tile. */}
   choose(r, c) {
-    // Set the new board.
+    {/* Set the new board. */}
     let newBoard = this.state.board.slice();
-    // Increment the number of clicks in the new state.
+    {/* Increment the number of clicks in the new state. */}
     let newClicks = this.state.clicks ++;
     let sRow = -1;
     let sCol = -1;
-    // Look through the board to see if there's a previously selected tile.
+    {/* Look through the board to see if there's a previously selected tile. */}
     for (var rowIndex = 0; rowIndex < newBoard.length; rowIndex++) {
       for (var colIndex = 0; colIndex < newBoard[rowIndex].length; colIndex++) {
-        // If one exists,
+        {/* If one exists, */}
         if (newBoard[rowIndex][colIndex].selected) {
-          // Set sRow and sCol to the row and col values of the tile.
+          {/* Set sRow and sCol to the row and col values of the tile. */}
           sRow = rowIndex;
           sCol = colIndex;
         }
       }
-      // If there exists a previously selected tile,
+      {/* If there exists a previously selected tile, */}
       if (sRow > -1 && sCol > -1) {
-        // If the letter of the chosen tile is the same as the previously selected tile,
+        {/* If the letter of the chosen tile is the same as the previously selected tile, */}
         if (newBoard[r][c].letter === newBoard[sRow][sCol].letter) {
-          // The previously selected tile is no longer selected.
+          {/* The previously selected tile is no longer selected. */}
           newBoard[sRow][sCol].selected = false;
-          // The previously selected tile is now matched.
+          {/* The previously selected tile is now matched. */}
           newBoard[sRow][sCol].matched = true;
-          // The chosen tile is now matched.
+          {/* The chosen tile is now matched. */}
           newBoard[r][c].matched = true;
-          // If they don't match,
+          {/* If they don't match, */}
         } else {
-          // The previously selected tile is no longer selected.
+          {/* The previously selected tile is no longer selected. */}
           newBoard[sRow][sCol].selected = false;
-          // Alert the player that the letters don't match.
+          {/* Alert the player that the letters don't match. */}
           alert(newBoard[r][c].letter + "!=" + newBoard[sRow][sCol].letter);
         }
-        // If no tile has been previously selected,
+        {/* If no tile has been previously selected, */}
       } else {
-        // The chosen tile is now selected.
+        {/* The chosen tile is now selected. */}
         newBoard[r][c].selected = true;
       }
     }
-    // Update the state with the new board and number of clicks.
+    {/* Update the state with the new board and number of clicks. */}
     this.setState({ board: newBoard, clicks: newClicks });
   }
 
-  // Restart the game.
+  {/* Restart the game. */}
   restart() {
-    // Refresh the window.
+    {/* Refresh the window. */}
     window.location.reload();
   }
 
-  // Render the board.
+  {/* Render the board. */}
   render() {
-    // Render each of the rows in the board.
+    {/* Render each of the rows in the board. */}
     let board = _.map(this.state.board, (row, rowIndex) => {
       return <ShowRow
       key={rowIndex}
@@ -166,7 +166,7 @@ class Starter extends React.Component {
       choose={this.choose.bind(this)}
       row={row} />;
     });
-    // Return the HTML.
+    {/* Return the HTML. */}
     return (
       <div>
         <div className="row">
@@ -182,31 +182,31 @@ class Starter extends React.Component {
   }
 }
 
-// Render a row in the board.
+{/* Render a row in the board. */}
 function ShowRow(props) {
-  // Render each tile in the row.
+  {/* Render each tile in the row. */}
   let renderedRow = _.map(props.row, (col, colIndex) => {
-    // If the tile is selected or matched,
+    {/* If the tile is selected or matched, */}
     if (col.selected || col.matched) {
-      // Return the tile showing its letter.
+      {/* Return the tile showing its letter. */}
       return (
         <div className="column" key={colIndex}>
           <p>
-            // The tile has no onClick function because selected and matched tiles
-            // cannot be chosen again.
+            {/* The tile has no onClick function because selected and matched tiles
+                cannot be chosen again. */}
             <button>
               {col.letter}
             </button>
           </p>
         </div>
       );
-      // Otherwise,
+      {/* Otherwise, */}
     } else {
-      // Return the tile hiding its letter.
+      {/* Return the tile hiding its letter. */}
       return (
         <div className="column" key={colIndex}>
           <p>
-          // When clicked, the tile will choose itself.
+          {/* When clicked, the tile will choose itself. */}
             <button onClick={() => props.choose(props.rowIndex, colIndex)}>
               ?
             </button>
@@ -215,6 +215,6 @@ function ShowRow(props) {
       );
     }
   });
-  // Return the HTML.
+  {/* Return the HTML. */}
   return <div className="row">{renderedRow}</div>
 }
