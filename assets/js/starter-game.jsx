9f83d3fe408ca2a772dd1320db49fd268f8cdd26
Ben Romine <elementnumber35@gmail.com>
2019-01-31 13:11:31 -0500

    Added clickable to game state and game over checking

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -26,6 +26,7 @@ class Starter extends React.Component {
 
     // Set the state of the board.
     this.state = {
+      clickable: true,
       clicks: 0,
       score: 0,
       board: [
@@ -121,55 +122,81 @@ class Starter extends React.Component {
     };
   }
 
-  // Call this function when choosing a tile.
-  choose(r, c) {
-    // Set the new board.
-    let newBoard = this.state.board.slice();
-    // Increment the number of clicks in the new state.
-    let newClicks = this.state.clicks + 1;
-    let sRow = -1;
-    let sCol = -1;
-    // Look through the board to see if there's a previously selected tile.
-    for (var rowIndex = 0; rowIndex < newBoard.length; rowIndex++) {
-      for (var colIndex = 0; colIndex < newBoard[rowIndex].length; colIndex++) {
-        // If one exists,
-        if (newBoard[rowIndex][colIndex].selected) {
-          // Set sRow and sCol to the row and col values of the tile.
-          sRow = rowIndex;
-          sCol = colIndex;
+  // Call this to determine whether or not the game is over.
+  isGameOver() {
+    // Assume that  we're done with the game.
+    let done = true;
+    let tempBoard = this.state.board.slice();
+    // Look through every element in the board.
+    for (var rowIndex = 0; rowIndex < tempBoard.length; rowIndex++) {
+      for (var colIndex = 0; colIndex < tempBoard[rowIndex].length; colIndex++) {
+        // If an element is not matched,
+        if (!tempBoard[rowIndex][colIndex].matched) {
+          // We are not done with the game.
+          done = false;
         }
       }
     }
-    // Mark the currently chosen tile as selected for display purposes.
-    newBoard[r][c].selected = true;
-    // Set the state with the new board and number of clicks.
-    this.setState({ board: newBoard, clicks: newClicks });
-    // Wait 3 seconds, and then...
-    setTimeout(function () {
-      // If there exists a previously selected tile,
-      if (sRow > -1 && sCol > -1) {
-        // If the letter of the chosen tile is the same as the previously selected tile,
-        if (newBoard[r][c].letter === newBoard[sRow][sCol].letter) {
-          // The previously selected tile is no longer selected.
-          newBoard[sRow][sCol].selected = false;
-          // The currently chosen tile is no longer selected.
-          newBoard[r][c].selected = false;
-          // The previously selected tile is now matched.
-          newBoard[sRow][sCol].matched = true;
-          // The chosen tile is now matched.
-          newBoard[r][c].matched = true;
+    // If we're still done with the game,
+    if (done) {
+      this.setState({ score: 100 - (x - 16) });
+    }
+  }
 
-          // If they don't match,
-        } else {
-          // The previously selected tile is no longer selected.
-          newBoard[sRow][sCol].selected = false;
-          // The currently chosen tile is no longer selected.
-          newBoard[r][c].selected = false;
+  // Call this function when choosing a tile.
+  choose(r, c) {
+    // If we can click,
+    if (this.state.clickable) {
+      // Set the new board.
+      let newBoard = this.state.board.slice();
+      // Increment the number of clicks in the new state.
+      let newClicks = this.state.clicks + 1;
+      let sRow = -1;
+      let sCol = -1;
+      // Look through the board to see if there's a previously selected tile.
+      for (var rowIndex = 0; rowIndex < newBoard.length; rowIndex++) {
+        for (var colIndex = 0; colIndex < newBoard[rowIndex].length; colIndex++) {
+          // If one exists,
+          if (newBoard[rowIndex][colIndex].selected) {
+            // Set sRow and sCol to the row and col values of the tile.
+            sRow = rowIndex;
+            sCol = colIndex;
+          }
         }
-        // Update the state with the new board.
-        this.setState({ board: newBoard });
       }
-    }.bind(this), 1000);
+      // Mark the currently chosen tile as selected for display purposes.
+      newBoard[r][c].selected = true;
+      // Set the state with the new board and number of clicks.
+      this.setState({ board: newBoard, clicks: newClicks, clickable: false });
+      // Wait 3 seconds, and then...
+      setTimeout(function () {
+        // If there exists a previously selected tile,
+        if (sRow > -1 && sCol > -1) {
+          // If the letter of the chosen tile is the same as the previously selected tile,
+          if (newBoard[r][c].letter === newBoard[sRow][sCol].letter) {
+            // The previously selected tile is no longer selected.
+            newBoard[sRow][sCol].selected = false;
+            // The currently chosen tile is no longer selected.
+            newBoard[r][c].selected = false;
+            // The previously selected tile is now matched.
+            newBoard[sRow][sCol].matched = true;
+            // The chosen tile is now matched.
+            newBoard[r][c].matched = true;
+            this.isGameOver();
+            // If they don't match,
+          } else {
+            // The previously selected tile is no longer selected.
+            newBoard[sRow][sCol].selected = false;
+            // The currently chosen tile is no longer selected.
+            newBoard[r][c].selected = false;
+          }
+          // Update the state with the new board.
+          this.setState({ board: newBoard });
+        }
+        // Re-enable clicks.
+        this.setState({ clickable: true });
+      }.bind(this), 1000);
+    }
   }
 
   // Restart the game.
